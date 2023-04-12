7ab318c1f104b33961afc158378a276565cab9b1
alejandro <alejandroj.zavala3@gmail.com>
2023-04-12 16:33:24 -0600

    fixing enviroment for pages working

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return <>
-  <Outlet
-  </>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 };
 
 export default Layout;
